f065eae0b97b4f9f445c626fd60073b6677f3715
Moro933978 <omar6789123h@gmail.com>
2026-05-24 13:54:50 +0300

    Updatefb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -66,7 +66,7 @@ const Hero = () => {
 
                         {/* Title */}
                         <div className="space-y-2" data-aos="fade-up" data-aos-delay="600">
-                            <h1 className="text-5xl sm:text-6xl md:text-6xl lg:text-6xl xl:text-7xl font-bold tracking-wide">
+                            <h1 className="text-5xl sm:text-6xl md:text-6xl lg:text-6xl xl:text-7xl font-black tracking-normal">
                                 <span className="relative inline-block">
                                     <span className="absolute -inset-2 bg-gradient-to-r from-[#6366f1] to-[#a855f7] blur-2xl opacity-20"></span>
                                     <span className="relative bg-gradient-to-r from-white via-blue-100 to-purple-200 bg-clip-text text-transparent">
